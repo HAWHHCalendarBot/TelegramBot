aecfedfa8045545a45d4694e4b58cb095e2ef073
Edgar Toll <edjopato@gmail.com>
2017-08-11 01:32:58 +0200

    feat(bot): add /about command

--- parts/start.js
+++ parts/start.js
@@ -18,9 +18,12 @@ bot.command(['start', 'url'], ctx => {
     keyboardKeys.push(Markup.urlButton('Kalender abonnieren', `https://calendarbot.hawhh.de/ics.php?url=${url}`))
     keyboardKeys.push(Markup.urlButton('Kalender URL', `https://${url}`))
   }
+  text += 'Mit /about kannst du mehr über diesen Bot erfahren.'
   text += '\nUnter /settings gibt es genauere Einstellungen zu deinem Kalender oder diesem Bot.'
 
   ctx.reply(text, Markup.inlineKeyboard(keyboardKeys, {
     columns: 1
   }).extra())
 })
+
+bot.command('about', ctx => ctx.replyWithMarkdown('Die Funktionsweise dieses Bots wird auf [calendarbot.hawhh.de](https://calendarbot.hawhh.de) genauer beschrieben.\n\nWenn dir der Bot gefällt, dann empfehle ihn gern weiter!\n\nDu hast Probleme, Ideen oder Vorschläge, was der Bot können sollte? Dann wende dich an @EdJoPaTo oder erstelle ein Issue auf [GitHub](https://github.com/HAWHHCalendarBot/telegrambot/issues).'))
