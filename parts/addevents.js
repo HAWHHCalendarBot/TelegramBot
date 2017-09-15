41b06b2f78ebc5b57d20e84e14d5b9dc1fd5ccd1
Edgar Toll <edjopato@gmail.com>
2017-09-15 03:32:53 +0200

    feat: add stats command

--- parts/addevents.js
+++ parts/addevents.js
@@ -138,3 +138,15 @@ bot.action(/a:(.+)/, async ctx => {
 
   return ctx.answerCallbackQuery(`${event} wurde zu deinem Kalender hinzugefügt.`)
 })
+
+bot.command('stats', async ctx => {
+  const userIds = await ctx.userconfig.allIds()
+  const userCount = userIds.length
+  const eventCount = allEvents.length
+
+  let text = `Ich habe aktuell ${eventCount} Veranstaltungen, die ich ${userCount} begeisterten Nutzern 😍 zur Verfügung stelle. Die letzte Nachricht habe ich gerade eben von dir erhalten.`
+  text += '\nWenn ich für dich hilfreich bin, dann erzähl gern anderen von mir, denn ich will gern allen helfen, denen noch zu helfen ist. ☺️'
+  text += '\n\nWenn du noch mehr über meine Funktionsweise wissen willst: /about'
+
+  return ctx.replyWithMarkdown(text)
+})
