a36a3dac75ef7ffe031be90d30acff64a64aeb17
Edgar Toll <edjopato@gmail.com>
2017-09-17 21:36:32 +0200

    refactor: bot.use checks for a middleware of the passed object
    the require is not that specific and the original object that was
    imported can be used besides the use of the middleware

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const { Extra, Markup } = Telegraf
 const hasStISysChanged = require('./hasStISysChanged.js')
 const Chatconfig = require('./lib/chatconfig.js')
 
-const addevents = require('./parts/addevents.js').middleware()
-const admin = require('./parts/admin.js').middleware()
-const easterEggs = require('./parts/easterEggs.js').middleware()
-const events = require('./parts/events.js').middleware()
-const settings = require('./parts/settings.js').middleware()
-const start = require('./parts/start.js').middleware()
+const addevents = require('./parts/addevents.js')
+const admin = require('./parts/admin.js')
+const easterEggs = require('./parts/easterEggs.js')
+const events = require('./parts/events.js')
+const settings = require('./parts/settings.js')
+const start = require('./parts/start.js')
 
 const token = fs.readFileSync(process.env.npm_package_config_tokenpath, 'utf8').trim()
 const bot = new Telegraf(token)
