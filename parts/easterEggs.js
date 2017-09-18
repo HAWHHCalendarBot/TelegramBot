784b01cb2916b2d7a1c4f9e100879e15eda73e33
Edgar Toll <edjopato@gmail.com>
2017-09-18 09:51:56 +0200

    feat(easteregg): reply to edited message

--- parts/easterEggs.js
+++ parts/easterEggs.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
+const { Extra } = Telegraf
 
 const bot = new Telegraf.Composer()
 module.exports = bot
 
-bot.on('edited_message', ctx => ctx.reply('Hui, jetzt wirds stressig. 😨\n\nIch kann doch nicht auch noch auf vergangene Nachrichten aufpassen!'))
+bot.on('edited_message', ctx => ctx.reply('Hui, jetzt wirds stressig. 😨\n\nIch kann doch nicht auch noch auf vergangene Nachrichten aufpassen!', Extra.inReplyTo(ctx.editedMessage.message_id)))
