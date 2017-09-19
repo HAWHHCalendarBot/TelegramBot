0b4b00eb6f54de46321f2d51c0145832c03df8e5
Edgar Toll <edjopato@gmail.com>
2017-09-19 18:52:53 +0200

    feat(start): create a more clean looking main menu

--- parts/start.js
+++ parts/start.js
@@ -7,20 +7,27 @@ module.exports = bot
 
 bot.command('start', ctx => {
   let text = `Hey ${ctx.from.first_name}!`
-  text += '\n\nMit /add kannst du Veranstaltungen zu deinem Kalender hinzufügen.'
+
+  text += '\n'
+  text += '\n*Kalender*'
+  text += '\nMit /add kannst du Veranstaltungen zu deinem Kalender hinzufügen.'
 
   if (ctx.state.userconfig.events.length > 0) {
     text += '\nMit /remove kannst du Veranstaltungen entfernen.'
     text += '\nMit /list kannst du die Liste deiner Veranstaltungen einsehen.'
     text += '\nUnter /subscribe findest du Tipps, wie du den Kalender immer up to date auf dein bevorzugtes Gerät bekommst.'
   }
-  text += '\nMit /about kannst du mehr über diesen Bot erfahren.'
-  text += '\nUnter /settings gibt es genauere Einstellungen zu deinem Kalender oder diesem Bot.'
 
   text += '\n'
+  text += '\n*Mensa*'
   text += '\nDas Mensaangebot erreichst du mit /mensa.'
 
-  ctx.reply(text)
+  text += '\n'
+  text += '\n*Sonstiges*'
+  text += '\nMit /about kannst du mehr über diesen Bot erfahren.'
+  text += '\nUnter /settings gibt es Einstellungen dieses Bots.'
+
+  ctx.replyWithMarkdown(text)
 })
 
 bot.command('about', ctx => ctx.replyWithMarkdown('Die Funktionsweise dieses Bots wird auf [calendarbot.hawhh.de](https://calendarbot.hawhh.de) genauer beschrieben.\n\nWenn dir der Bot gefällt, dann empfehle ihn gern weiter!\n\nDu hast Probleme, Ideen oder Vorschläge, was der Bot können sollte? Dann wende dich an @EdJoPaTo oder erstelle ein Issue auf [GitHub](https://github.com/HAWHHCalendarBot/telegrambot/issues).', Extra.markup(Markup.removeKeyboard())))
