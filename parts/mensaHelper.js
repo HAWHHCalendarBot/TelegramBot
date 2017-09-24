89bf28100abb93080003c75acfd70e57afd692e3
Edgar Toll <edjopato@gmail.com>
2017-09-24 12:11:03 +0200

    feat(mensa): add noBeef and noPoultry filter
    closes #5

--- parts/mensaHelper.js
+++ parts/mensaHelper.js
@@ -1,7 +1,9 @@
 function filterMeals(meals, specialWishes) {
   return meals
-    .filter(m => !specialWishes.noPig || !m.Pig)
+    .filter(m => !specialWishes.noBeef || !m.Beef)
     .filter(m => !specialWishes.noFish || !m.Fish)
+    .filter(m => !specialWishes.noPig || !m.Pig)
+    .filter(m => !specialWishes.noPoultry || !m.Poultry)
     .filter(m => !specialWishes.lactoseFree || m.LactoseFree)
     .filter(m => !specialWishes.vegan || m.Vegan)
     .filter(m => !specialWishes.vegetarian || m.Vegan || m.Vegetarian)
@@ -12,6 +14,8 @@ function mensaSpecialWishesButtons(specialWishes) {
   if (!specialWishes.vegan) {
     if (!specialWishes.vegetarian) {
       possibleSettings.push('noPig')
+      possibleSettings.push('noBeef')
+      possibleSettings.push('noPoultry')
       possibleSettings.push('noFish')
     }
     possibleSettings.push('lactoseFree')
