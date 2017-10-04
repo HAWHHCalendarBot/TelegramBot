f94411642cb11ec89c346e5c7216d6235a868a4e
Edgar Toll <edjopato@gmail.com>
2017-10-04 20:27:15 +0200

    feat(easteregg): leave channels the bot was added to
    The Channel @devariran added my dev bot as an admin to their channel…
    dont know why.

--- parts/easterEggs.js
+++ parts/easterEggs.js
@@ -5,3 +5,9 @@ const bot = new Telegraf.Composer()
 module.exports = bot
 
 bot.on('edited_message', ctx => ctx.reply('Hui, jetzt wirds stressig. 😨\n\nIch kann doch nicht auch noch auf vergangene Nachrichten aufpassen!', Extra.inReplyTo(ctx.editedMessage.message_id)))
+
+bot.on('channel_post', async ctx => {
+  await ctx.reply('Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.')
+  console.log(`leave the channel…`)
+  return ctx.leaveChat(ctx.chat.id)
+})
