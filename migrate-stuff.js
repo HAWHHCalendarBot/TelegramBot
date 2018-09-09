0d9b18ff16704694b29bb6b26a9d4f768fbe277b
Edgar To <edjopato@gmail.com>
2018-09-09 13:39:57 +0200

    feat(migrate): remove anomaly in userconfig

--- migrate-stuff.js
+++ migrate-stuff.js
@@ -11,6 +11,19 @@ bot.use((ctx, next) => {
       ctx.state.userconfig.mensa.price = 'guest'
     }
     delete ctx.state.userconfig.mensa.student
+
+    // Sometimes people have the main mensa multiple times in the more list
+    // Maybe I fixed that bug with 0b25d01, but havnt checked yet
+    const {main, more} = ctx.state.userconfig.mensa
+    if (main && more && more.length > 0) {
+      const beforeCount = more.length
+      ctx.state.userconfig.mensa.more = more.filter(o => o !== main)
+      const afterCount = ctx.state.userconfig.mensa.more.length
+
+      if (beforeCount !== afterCount) {
+        console.log('removed main mensa in more', ctx.from)
+      }
+    }
   }
 
   return next()
