79d8ae5b596ebf4672955b0e3886a3a9679a3ff4
Edgar To <edjopato@gmail.com>
2018-09-09 07:55:56 +0200

    feat(events): add list to new event menu

--- parts/events.js
+++ parts/events.js
@@ -2,7 +2,28 @@ const TelegrafInlineMenu = require('telegraf-inline-menu')
 
 const addMenu = require('./events-add')
 
-const menu = new TelegrafInlineMenu('e', 'Hier hast du einen Überblick über deine Veranstaltungen')
+function overviewText(ctx) {
+  let text = 'Hier hast du einen Überblick über deine Veranstaltungen'
+
+  const {events, additionalEvents} = ctx.state.userconfig
+  if (events.length > 0) {
+    text += '\n\nDu hast folgende Veranstaltungen im Kalender:\n'
+    const eventLines = events.map(o => '- ' + o)
+    text += eventLines.join('\n')
+  } else {
+    text += '\n\nDu hast aktuell keine Veranstaltungen in deinem Kalender. 😔'
+  }
+
+  if ((additionalEvents || []).length > 0) {
+    text += '\n\nUnd du Veranstalter:\n'
+    const eventLines = additionalEvents.map(o => '- ' + o)
+    text += eventLines.join('\n')
+  }
+
+  return text
+}
+
+const menu = new TelegrafInlineMenu('e', overviewText)
 
 menu.submenu('Hinzufügen…', addMenu.menu)
 
