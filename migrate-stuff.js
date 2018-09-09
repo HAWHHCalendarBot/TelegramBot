8cdc040255f423b00417398088d6fd0e2aa93e62
Edgar To <edjopato@gmail.com>
2018-09-09 07:55:27 +0200

    fix(migrate): migrate only when mensa settings is there

--- migrate-stuff.js
+++ migrate-stuff.js
@@ -4,12 +4,14 @@ const bot = new Telegraf.Composer()
 
 bot.use((ctx, next) => {
   // Mirgrate Mensa Student Price
-  if (ctx.state.userconfig.mensa.student) {
-    ctx.state.userconfig.mensa.price = 'student'
-  } else if (!ctx.state.userconfig.mensa.price) {
-    ctx.state.userconfig.mensa.price = 'guest'
+  if (ctx.state.userconfig.mensa) {
+    if (ctx.state.userconfig.mensa.student) {
+      ctx.state.userconfig.mensa.price = 'student'
+    } else if (!ctx.state.userconfig.mensa.price) {
+      ctx.state.userconfig.mensa.price = 'guest'
+    }
+    delete ctx.state.userconfig.mensa.student
   }
-  delete ctx.state.userconfig.mensa.student
 
   return next()
 })
