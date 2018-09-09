0b28dbdd75a6d6200eae8d71b1b982f338494dcb
Edgar To <edjopato@gmail.com>
2018-09-09 16:08:20 +0200

    refactor(mensa): move mealToMarkdown into lib

--- parts/mensa.js
+++ parts/mensa.js
@@ -2,7 +2,7 @@ const fsPromises = require('fs').promises
 
 const Telegraf = require('telegraf')
 
-const {filterMeals} = require('../lib/mensa-helper')
+const {filterMeals, mealToMarkdown} = require('../lib/mensa-helper')
 
 const {Extra, Markup} = Telegraf
 
@@ -30,57 +30,7 @@ bot.use((ctx, next) => {
   return next()
 })
 
-function mealToMarkdown(meal, priceClass, showAdditives) {
-  const parsedName = showAdditives ?
-    meal.Name
-      .replace(/ \(/g, '* (')
-      .replace(/\), /g, '), *')
-      .replace(/([^)])$/, '$1*') :
-    meal.Name.replace(/\s*\([^)]+\)\s*/g, '') + '*'
 
-  const price = priceClass === 'student' ? meal.PriceStudent : priceClass === 'attendant' ? meal.PriceAttendant : meal.PriceGuest
-  const priceStr = price.toLocaleString('de-DE', {minimumFractionDigits: 2})
-
-  let text = `*${parsedName}\n`
-  text += `${priceStr} €`
-
-  const infos = []
-
-  if (meal.Pig) {
-    infos.push('🐷')
-  }
-  if (meal.Beef) {
-    infos.push('🐮')
-  }
-  if (meal.Poultry) {
-    infos.push('🐔')
-  }
-  if (meal.Fish) {
-    infos.push('🐟')
-  }
-
-  if (meal.LactoseFree) {
-    infos.push('laktosefrei')
-  }
-  if (meal.Vegan) {
-    infos.push('vegan')
-  }
-  if (meal.Vegetarian) {
-    infos.push('vegetarisch')
-  }
-
-  if (infos.length > 0) {
-    text += ' ' + infos.join(' ')
-  }
-
-  if (showAdditives) {
-    for (const additive of meal.Additives) {
-      text += `\n${additive.Key}: ${additive.Value}`
-    }
-  }
-
-  return text
-}
 
 const weekdays = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
 async function mensaText(mensa, year, month, day, mensaSettings) {
