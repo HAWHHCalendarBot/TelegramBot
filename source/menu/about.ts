02516c43abebb46584585d71c5e8edb8ba14bff9
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-04-06 00:29:07 +0200

    refactor(about): use formatting helper functions

--- source/menu/about.ts
+++ source/menu/about.ts
@@ -1,12 +1,22 @@
 import {MenuTemplate, Body} from 'telegraf-inline-menu'
+import {html as format} from 'telegram-format'
 
 import {backMainButtons} from '../lib/inline-menu.js'
 
 export const menu = new MenuTemplate(menuBody)
 
 function menuBody(): Body {
-	const text = 'Die Funktionsweise dieses Bots wird auf [calendarbot.hawhh.de](https://calendarbot.hawhh.de) genauer beschrieben.\n\nWenn dir der Bot gefällt, dann empfehle ihn gern weiter!\n\nDu hast Probleme, Ideen oder Vorschläge, was der Bot können sollte? Dann wende dich an @EdJoPaTo oder erstelle ein Issue auf [GitHub](https://github.com/HAWHHCalendarBot/telegrambot/issues).'
-	return {text, parse_mode: 'Markdown'}
+	const websiteLink = format.url('calendarbot.hawhh.de', 'https://calendarbot.hawhh.de')
+	const githubIssues = format.url('GitHub', 'https://github.com/HAWHHCalendarBot/telegrambot/issues')
+
+	let text = ''
+	text += `Die Funktionsweise dieses Bots wird auf ${websiteLink} genauer beschrieben.`
+	text += '\n\n'
+	text += 'Wenn dir der Bot gefällt, dann empfehle ihn gern weiter!'
+	text += '\n\n'
+	text += `Du hast Probleme, Ideen oder Vorschläge, was der Bot können sollte? Dann wende dich an @EdJoPaTo oder erstelle ein Issue auf ${githubIssues}.`
+
+	return {text, parse_mode: format.parse_mode}
 }
 
 menu.manualRow(backMainButtons)
